2fcd812f4ac14bf5c86fc49d76f99cd527fccafb
Yannick Wittwer <wittwer.yannick@gmx.ch>
2020-12-10 13:41:28 +0100

    improve configurations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'app/styles')],
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
   },
 };
